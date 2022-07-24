e9c32b1e5bedd1c944b3a01348c4b28b88c35333
Robin Liu <robinliu.lloii@gmail.com>
2022-07-24 08:30:53 +0800

    feat: init springboot project

---
+++ src/main/java/com/example/multithreadtest/MultiThreadTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.multithreadtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiThreadTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiThreadTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/multithreadtest/MultiThreadTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multithreadtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiThreadTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
